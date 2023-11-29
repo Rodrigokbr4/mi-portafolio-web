45f711756ba578edf7900723d02fa181bad404ca
Rodrigo Cabrera <cabrerarodrigo481@gmail.com>
2023-11-28 21:13:05 -0300

    se añade animacion al cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import AnimatedCursor from 'react-animated-cursor'
 import { Header } from './components/Header/Header.jsx'
 import { BodyCards } from './components/BodyCards/BodyCards.jsx'
 
@@ -6,6 +7,36 @@ function App() {
 	return (
 		<>
 			<header className={styles.Header}>
+				<AnimatedCursor
+					showSystemCursor={true}
+					innerSize={54}
+					outerSize={65}
+					color='255, 255, 255'
+					outerAlpha={0.2}
+					innerScale={0.7}
+					outerScale={2}
+					trailingSpeed={5}
+					outerStyle={{
+						border: '3px solid var(--cursor-color)',
+					}}
+					innerStyle={{
+						backgroundColor: 'var(--cursor-color)',
+					}}
+					clickables={[
+						'a',
+						'input[type="text"]',
+						'input[type="email"]',
+						'input[type="number"]',
+						'input[type="submit"]',
+						'input[type="image"]',
+						'label[for]',
+						'select',
+						'textarea',
+						'button',
+						'.link',
+						'img',
+					]}
+				/>
 				<Header />
 				<BodyCards />
 			</header>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,29 @@ import perfil from '../../assets/Img/perfil.jpg'
 import { Contactos } from '../Contactos/Contactos'
 
 export function Header() {
+	const handleImageHover = () => {
+		document.querySelector('AnimatedCursor').style.display = 'none'
+	}
+
+	const handleImageLeave = () => {
+		document.querySelector('AnimatedCursor').style.display = 'block'
+	}
 	return (
 		<>
 			<div className={styles.Header}>
-				<img src={perfil} alt='Foto de perfil' className={styles.Img} />
-				<h1 className={styles.H1}>Rodrigo Cabrera</h1>
-				<h2 className={styles.H2}>Web Developer</h2>
-				<div className={styles.Contactos}>
-					<Contactos />
+				<img
+					src={perfil}
+					alt='Foto de perfil'
+					className={styles.Img}
+					onMouseEnter={handleImageHover}
+					onMouseLeave={handleImageLeave}
+				/>
+				<div className={styles.Env}>
+					<h1 className={styles.H1}>Rodrigo Cabrera</h1>
+					<h2 className={styles.H2}>Web Developer</h2>
+					<div className={styles.Contactos}>
+						<Contactos />
+					</div>
 				</div>
 			</div>
 		</>
